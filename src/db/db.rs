a6ecc923baa69d5f2508a952b288ab8318009b9e
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2023-04-15 23:51:11 -0600

    added windows file ending check

--- src/db/db.rs
+++ src/db/db.rs
@@ -1,5 +1,7 @@
+// use rocket::log::private::info;
 use rusqlite::{Connection};
-use std::{fs, path::Path};
+use std::{env,fs, path::Path};
+use log::{info, error};
 
 pub fn connect() -> Connection {
     let mut must_initialize_db = false;
@@ -7,16 +9,27 @@ pub fn connect() -> Connection {
         must_initialize_db = true;
     }
 
-    let connection = Connection::open("dd.db").unwrap();
+    let connection = Connection::open("dd.db").unwrap_or_else(|e| {
+        error!(target: "error", "failed to open database: {}", e);
+        panic!("database access error")
+    });
     
     if must_initialize_db {
+        let line_ending = match env::consts::OS {
+            "windows" => ";\r\n",
+            _ => ";\n",
+        };
         let query = fs::read_to_string("init.sql").expect("initial schema does not exist");
-        let commands = query.split(";\n");
+        let commands = query.split(line_ending);
 
         for command in commands {
-            connection.execute(command, ()).unwrap();
+            connection.execute(command, ()).unwrap_or_else(|e| {
+                error!("failed to execute command '{}': {}", command, e);
+                panic!("command execution failure")
+            });
         }
+        
+        info!(target: "info", "database created");
     }
-    
     return connection;
 }
