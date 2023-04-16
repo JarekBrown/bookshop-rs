b034684db46c59f82492342f6521563e00e2321a
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2023-04-15 23:51:56 -0600

    logger init logic file

---
+++ src/logging.rs
@@ -0,0 +1,10 @@
+use log4rs;
+
+pub fn log_init() {
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap_or_else(|error| {
+        println!(
+            "ERROR: problem encountered when initializing logger -- {}",
+            error
+        )
+    });
+}
