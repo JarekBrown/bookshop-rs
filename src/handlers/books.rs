a8dafe7329b7385f655cce183a8ff6263ff7c73a
Jarek Brown <31749343+JarekBrown@users.noreply.github.com>
2023-04-16 20:56:57 -0600

    added standardizers for books

--- src/handlers/books.rs
+++ src/handlers/books.rs
@@ -1,6 +1,9 @@
 use crate::db::books;
+use log::{error, info, warn};
+use regex::Regex;
 use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
+use titlecase::titlecase; // standardizes inputs to titlecase
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Book {
@@ -9,23 +12,15 @@ pub struct Book {
     author: Option<String>,
     price: Option<f64>,
 }
+impl Book {}
 
 #[post("/new", data = "<book>")]
 pub fn create_book(book: Json<Book>) -> Result<(), String> {
-    let title = match book.title.clone() {
-        Some(t) => t,
-        None => return Err("No title provided".to_string()),
-    };
-    let author = match book.author.clone() {
-        Some(a) => a,
-        None => return Err("No author provided".to_string()),
-    };
-    let price = match book.price.clone() {
-        Some(p) => p,
-        None => return Err("No price provided".to_string()),
-    };
+    let title = validate_title(book.title.clone())?;
+    let author = validate_auth(book.author.clone())?;
+    let price = validate_price(book.price.clone())?;
 
-    books::create_book(title, author, price);
+    books::create_book(title, author, price)?;
     Ok(())
 }
 
@@ -35,21 +30,71 @@ pub fn create_book(book: Json<Book>) -> Result<(), String> {
 // sense in my mind
 #[get("/price", format = "json", data = "<book>")]
 pub fn get_price(book: Json<Book>) -> Result<Json<Book>, String> {
-    let title = match book.title.clone() {
-        Some(t) => t,
-        None => return Err("No title provided".into()),
-    };
-    let author = match book.author.clone() {
-        Some(a) => a,
-        None => return Err("No author provided".into()),
-    };
+    let title = validate_title(book.title.clone())?;
+    let author = validate_auth(book.author.clone())?;
 
-    let bid = books::get_book_id(title, author);
-    let price = books::get_book_price(bid);
+    let bid = books::get_book_id(title.clone(), author.clone())?;
+    let price = books::get_book_price(bid)?;
     Ok(Json(Book {
-        id: None,
-        title: None,
-        author: None,
+        id: Some(bid),
+        title: Some(title),
+        author: Some(author),
         price: Some(price),
     }))
+}
+
+fn validate_title(title: Option<String>) -> Result<String, String> {
+    //! validation function for title field (unwraps Option<String>)
+    let title = match title {
+        Some(s) => s,
+        None => {
+            warn!(target: "warn", "title name validation failed: no title provided");
+            return Err("no title provided".to_string());
+        }
+    };
+    let re = Regex::new(r"\s+").expect("regex creation failed");
+    let title = re.replace_all(&title, " ").to_string();
+    if !title
+        .chars()
+        .all(|c| c.is_alphanumeric() || c.to_string() == " ")
+    {
+        warn!(target: "warn", "provided title is not alphanumeric");
+        Err("title should be alpha-numeric".to_string())
+    } else {
+        Ok(titlecase(&title.to_lowercase()))
+    }
+}
+
+fn validate_auth(author: Option<String>) -> Result<String, String> {
+    //! validation function for author field (unwraps Option<String>)
+    let auth = match author {
+        Some(s) => s,
+        None => {
+            warn!(target: "warn", "author name validation failed: no author provided");
+            return Err("no author provided".to_string());
+        }
+    };
+    let re = Regex::new(r"\s+").expect("regex creation failed");
+    let auth = re.replace_all(&auth, " ").to_string();
+    if !auth
+        .chars()
+        .all(|c| c.is_alphanumeric() || c.to_string() == " ")
+    {
+        warn!(target: "warn", "provided author is not alphanumeric");
+        Err("author should be alpha-numeric".to_string())
+    } else {
+        Ok(titlecase(&auth.to_lowercase()))
+    }
+}
+
+fn validate_price(price: Option<f64>) -> Result<f64, String> {
+    let price = match price {
+        Some(s) => s,
+        None => return Err("no price provided".to_string()),
+    };
+    if price >= 0.01 {
+        Ok(price)
+    } else {
+        Err("price must be greater than 0".to_string())
+    }
 }
